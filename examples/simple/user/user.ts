74d73a6a6a2f4b9b3e3ced7adb8ddde005deca9a
Daniel Lewis <dan.lewis@enspiral.com>
2025-07-18 19:45:12 +0200

    woops shouldn't commit test changes

--- examples/simple/user/user.ts
+++ examples/simple/user/user.ts
@@ -3,7 +3,6 @@ export interface User {
   name: string;
   email: string;
 }
-hello
 
 export function getUser(id: string): User | null {
   // In a real application, you would fetch this from a database
